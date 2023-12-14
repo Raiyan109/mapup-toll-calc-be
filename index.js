08585a486d83ffb9d9ba4e03868ac26ae69a4f80
Raiyan109 <kraiyan109@gmail.com>
2023-12-14 14:33:46 +0600

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors');
+const { default: axios } = require('axios');
 require('dotenv').config()
 
 const app = express()
@@ -9,13 +10,33 @@ app.use(express.json())
 app.use(cors())
 
 
+app.post('/origin-destination', async (req, res) => {
+    try {
+        const response = await axios.post('https://apis.tollguru.com/toll/v2/origin-destination-waypoints',
+            req.body,
+            {
+                headers: {
+                    'x-api-key': 'jhPLDm64pPQtBTQnmMbt3NrMpQh2ngBg',
+                    'Content-Type': 'application/json',
+                },
+            })
+        res.status(200).json({
+            success: true,
+            data: response.data,
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error')
+    }
+})
+
 // mongoose.set("strictQuery", false);
 // mongoose.connect(process.env.MONGO_URI)
 //     .then(() => {
 app.listen(process.env.PORT, (req, res) => {
     console.log(`Server listening on ${process.env.PORT}`)
 })
-    // })
-    // .catch((error) => {
-    //     console.log(error)
-    // })
+// })
+// .catch((error) => {
+//     console.log(error)
+// })
